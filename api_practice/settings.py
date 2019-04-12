df9e584f462c63a15de7c034e56beaf227061587
patrick-wittybunny <patrick.wittybunny@gmail.com>
2019-04-12 16:01:30 +0800

    Update

--- api_practice/settings.py
+++ api_practice/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
